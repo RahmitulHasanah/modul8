8ebd6848aae3a2e1f82af6789b8ab2b48585defb
RahmitulHasanah <117124873+RahmitulHasanah@users.noreply.github.com>
2022-12-15 11:07:56 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul8_RahmitulHasanah;
+
+/**
+ *
+ * @author RIMTECH COMPUTER
+ */
+public class latihan1 {
+    public static void main(String[] args){
+        
+        //arguments = java Hello world that is all
+        
+        
+        int i;
+        for(i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
